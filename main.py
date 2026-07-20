6f66570a77932a3a586c8f6e5baffa117087f812
็GAME <thanaduangnamo@gmail.com>
2026-07-20 20:39:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
+import os
 import discord
 from discord.ext import commands
 
-# ตั้งค่า Intents พื้นฐาน
 intents = discord.Intents.default()
+intents.message_content = True 
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'🟢 บอทออนไลน์แล้วในชื่อ: {bot.user.name}')
-    
-    # 🔴 ตั้งค่าสถานะให้ออนเป็นสีแดง (Do Not Disturb)
+    print(f'🟢 บอทออนไลน์แล้ว: {bot.user.name}')
     await bot.change_presence(
         status=discord.Status.dnd, 
-        activity=discord.Game(name="ระบบเปิดใช้งาน 24 ชม.") # ข้อความใต้ชื่อบอท
+        activity=discord.Game(name="ระบบเปิดใช้งาน 24 ชม.")
     )
 
-# ใส่ Token บอทของคุณที่นี่
-bot.run('MTUyNjIxNDkxMzQ1MjgwMjEwOA.GPbNe9.Ez0mXlAUlLErQWngvBZwklXwXJEGO3vYBTJL54')
+bot.run(os.environ.get('DISCORD_TOKEN'))
